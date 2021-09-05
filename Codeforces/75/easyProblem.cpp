17862695232f0d72af44e14cd99fd595ad3f5adb
Bintee <80423437+MuhsinaBintee@users.noreply.github.com>
2021-09-06 04:02:11 +0600

    Add files via upload

---
+++ Codeforces/75/easyProblem.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,a,ans=0;
+   vector<int> v;
+   cin>>n;
+   for(int i=0;i<n;i++){
+    cin>>a;
+    v.push_back(a);
+   }
+
+   vector<int> ::iterator it;
+for(it=v.begin();it!=v.end();it++){
+    if(*it==1){
+        ans=1;
+        break;
+    }
+}
+if(ans==1){
+    cout<<"HARD"<<endl;
+}else{
+cout<<"EASY"<<endl;
+}
+
+
+return 0;
+}
