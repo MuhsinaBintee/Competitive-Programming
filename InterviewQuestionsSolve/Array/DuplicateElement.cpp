497d3e09bd497f438f25c58cda2ed9f9a933c891
Bintee <80423437+MuhsinaBintee@users.noreply.github.com>
2022-03-15 14:05:10 +0600

    update

--- InterviewQuestionsSolve/Array/DuplicateElement.cpp
+++ InterviewQuestionsSolve/Array/DuplicateElement.cpp
@@ -1,1 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,i,j,cnt=0;
+    cin>>n;
+    int a[n];
+    for(i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(a[i]==a[j]){
+                cout<<a[j]<<endl;
+                cnt++;
+            }
+        }
+    }
+    cout<<"Total duplicate element "<<cnt<<endl;
+
+
+return 0;
+}
 
