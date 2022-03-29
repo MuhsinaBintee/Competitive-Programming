b05ce75d2d2a86817f65b1c49695327ea40341f8
Bintee <80423437+MuhsinaBintee@users.noreply.github.com>
2022-03-29 21:00:59 +0600

    .

---
+++ Codeforces/codeforcesA/PickyEater.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    if(x-y>=0)
+    {
+        cout<<1<<endl;
+    }
+    else
+    {
+        cout<<0<<endl;
+    }
+    return 0;
+}
+
