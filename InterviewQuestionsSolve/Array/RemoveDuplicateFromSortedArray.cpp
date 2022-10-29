a9627163124946a778380998da17b89fc08f77c1
Bintee <80423437+MuhsinaBintee@users.noreply.github.com>
2022-10-29 15:50:35 +0600

    -

---
+++ InterviewQuestionsSolve/Array/RemoveDuplicateFromSortedArray.cpp
@@ -0,0 +1,35 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+void RemoveDuplicates(int a[],int n)
+{
+    int key = a[0];
+    int j=0;
+    for(int i=1; i<n; i++)
+    {
+        if(key != a[i])
+        {
+            a[j++]=key;
+            key=a[i];
+        }
+    }
+    a[j++]=key;
+    for(int i=0; i<j; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    RemoveDuplicates(a,n);
+    return 0;
+}
