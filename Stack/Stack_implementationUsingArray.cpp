fd8c71a636d1d5aa64d25a4d7499a6ef61937a18
Bintee <80423437+MuhsinaBintee@users.noreply.github.com>
2022-12-20 21:32:55 +0600

    stack implementation using array

---
+++ Stack/Stack_implementationUsingArray.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#define Mx_size 100
+using namespace std;
+int s[Mx_size];
+int top=-1;
+void push(int a){
+    if(top==Mx_size){
+        cout<<"Error : Stack overflow"<<endl;
+        return;
+    }else{
+        s[++top]=a;
+    }
+}
+void pop(){
+   if(top== -1){
+    cout<<"Error : Stack underflow"<<endl;
+    return;
+   }else{
+     top--;
+   }
+}
+int top(){
+  if(top==-1){
+    cout<<"Stack is empty"<<endl;
+    return;
+  }
+    return s[top];
+
+}
+void print(){
+  for(int i=0;i<=top;i++){
+    cout<<s[i]<<endl;
+  }
+}
+int main(){
+    push(2);
+    print();
+    push(5);
+    print();
+    push(10);
+    print();
+    pop();
+    print();
+    top();
+    print();
+    push(12);
+
+}
