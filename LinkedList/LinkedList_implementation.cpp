c91eb68eb6b1f031fc7bd8c66d47ced29e761d36
Bintee <80423437+MuhsinaBintee@users.noreply.github.com>
2023-01-30 22:58:00 +0600

    Create LinkedList_implementation.cpp

--- LinkedList/LinkedList_implementation.cpp
+++ LinkedList/LinkedList_implementation.cpp
@@ -1,1 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node * next;
+}*head;
+void CreateLinkedList(int n)
+{
+    struct Node *temp,*current;
+    int num;
+    head = (struct Node*)malloc(sizeof(struct Node));
+    cin>>num;
+    head->data=num;
+    head->next=NULL;
+    temp = head;
+    for(int i=2; i<=n; i++)
+    {
+        current = (struct Node*)malloc(sizeof(struct Node));
+        cin>>num;
+        current->data=num;
+        current->next=NULL;
 
+        temp->next=current;
+        temp=temp->next;
+
+    }
+}
+void display()
+{
+    struct Node *temp;
+    temp=head;
+    while(temp->next != NULL)
+    {
+        cout<<temp->data<<"->";
+        temp=temp->next;
+
+    }
+    cout<<temp->data<<endl;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    CreateLinkedList(n);
+    display();
+    return 0;
+}
