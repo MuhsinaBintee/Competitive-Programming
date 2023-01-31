bd63c444559f22cd95b47916ad3ed8d86a93587f
Bintee <80423437+MuhsinaBintee@users.noreply.github.com>
2023-01-31 17:17:06 +0600

    Create insertNodeAtPosition.cpp

---
+++ LinkedList/insertNodeAtPosition.cpp
@@ -0,0 +1,108 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node * next;
+}*head;
+void CreateLinkedList(int n)
+{
+    struct Node *temp,*current;
+    int num;
+    head = (struct Node*)malloc(sizeof(struct Node));
+    cin>>num;
+    head->data=num;
+    head->next=NULL;
+    temp = head;
+    for(int i=2; i<=n; i++)
+    {
+        current = (struct Node*)malloc(sizeof(struct Node));
+        cin>>num;
+        current->data=num;
+        current->next=NULL;
+
+        temp->next=current;
+        temp=temp->next;
+
+    }
+}
+void insertNodeBegining(int num)
+{
+    struct Node * newNode;
+    newNode = (struct Node *)malloc(sizeof(struct Node));
+
+    newNode->data= num;
+    newNode->next= head;
+    head= newNode;
+}
+void insertNodeAtPosition(int num,int pos)
+{
+    struct Node * newNode,*temp;
+    newNode=(struct Node*)malloc(sizeof(struct Node));
+    newNode->data=num;
+    newNode->next=NULL;
+    if(pos==1)
+    {
+        newNode->next=head;
+        head=newNode;
+    }
+    else
+    {
+
+        temp=head;
+        for(int i=2; i<pos; i++)
+        {
+            temp=temp->next;
+            if(temp==NULL)break;
+        }
+        newNode->next=temp->next;
+        temp->next=newNode;
+    }
+
+
+}
+void insertNodeAtEnd(int num)
+{
+    struct Node * newNode,*temp;
+    newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data=num;
+    newNode->next=NULL;
+    temp=head;
+    while(temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next=newNode;
+}
+void display()
+{
+    struct Node *temp;
+    temp=head;
+    while(temp->next != NULL)
+    {
+        cout<<temp->data<<"->";
+        temp=temp->next;
+
+    }
+    cout<<temp->data<<endl;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    CreateLinkedList(n);
+    display();
+    int headNode;
+    cin>>headNode;
+    insertNodeBegining(headNode);
+    display();
+    int newNode;
+    cin>>newNode;
+    insertNodeAtEnd(newNode);
+    display();
+    int nodeValue,pos;
+    cin>>nodeValue>>pos;
+    insertNodeAtPosition(nodeValue,pos);
+    display();
+    return 0;
+}
