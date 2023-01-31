83ea65dc216edd498cf36f7c2f17fad7684b77c6
Bintee <80423437+MuhsinaBintee@users.noreply.github.com>
2023-01-31 23:24:12 +0600

    Create SearchNodeFromLinkedList.cpp

---
+++ LinkedList/SearchNodeFromLinkedList.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+   int data;
+   struct Node * next;
+}*head;
+void createLinkedList(int n){
+   struct Node *newNode,*temp;
+   int num;
+   cin>>num;
+   head = (struct Node*)malloc(sizeof(struct Node));
+   head->data=num;
+   head->next=NULL;
+   temp= head;
+   for(int i=2;i<=n;i++){
+    newNode=(struct Node*)malloc(sizeof(struct Node));
+        cin>>num;
+        newNode->data=num;
+        newNode->next=NULL;
+
+        temp->next=newNode;
+        temp=temp->next;
+   }
+}
+void display(){
+  struct Node *temp;
+  temp=head;
+  while(temp->next!=NULL){
+    cout<<temp->data<<"->";
+    temp=temp->next;
+  }
+  cout<<temp->data<<endl;
+}
+int SearchNode(int item){
+  int index=1;
+  struct Node *temp;
+  temp=head;
+  while(temp->next!=NULL){
+    if(temp->data==item){
+        return index;
+    }
+    temp=temp->next;
+    index++;
+  }
+  return 0;
+}
+int main(){
+int n;
+cin>>n;
+createLinkedList(n);
+display();
+int item;
+cin>>item;
+int pos = SearchNode(item);
+cout<<pos<<endl;
+return 0;
+}
