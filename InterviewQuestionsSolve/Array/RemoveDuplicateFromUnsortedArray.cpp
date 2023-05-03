620a922faf2b08e4417e7cfb62fed9937e7ee995
Bintee <80423437+MuhsinaBintee@users.noreply.github.com>
2023-05-03 11:59:21 +0600

    Update RemoveDuplicateFromUnsortedArray.cpp

--- InterviewQuestionsSolve/Array/RemoveDuplicateFromUnsortedArray.cpp
+++ InterviewQuestionsSolve/Array/RemoveDuplicateFromUnsortedArray.cpp
@@ -33,3 +33,30 @@ int main()
     RemoveDuplicates(a,n);
     return 0;
 }
+//method 2
+/*
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+int a[n];
+for(int i=0;i<n;i++){
+    cin>>a[i];
+}
+
+for(int i=0;i<n-1;i++){
+    for(int j=i+1;j<n;j++){
+        if(a[i]==a[j]){
+            swap(a[j],a[n-1]);
+            n--;
+        }
+    }
+}
+for(int i=0;i<n;i++){
+    cout<<a[i]<<" ";
+}
+
+return 0;
+}
+*/
